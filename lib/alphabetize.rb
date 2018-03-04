6f7816eb5f24a3297f6877c0ece13666a4f4386a
Harim Choi <harim1206@gmail.com>
2018-03-04 18:09:01 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,14 +2,6 @@ require "pry"
 
 =begin
 arr.sort_by { |a| a.split("").map{ |char| ESPERANTO_ALPHABET.index(char) } }
-
-
-input = ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
-[m,i, ,a,m,a,s, ,v,i,n]
-m -> "16"
-[b,o,n,a,n]
-[3,4,3,6]
-[16,5,4,6]
 =end
 def esperanto
   esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
